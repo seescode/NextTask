38a877d23e7626397980cf39541efb55e2cdb36d
seescode <seesyong@gmail.com>
2013-09-30 22:31:00 -0400

    Adding TaskRepository

--- NextTask/Data/TaskRepository.cs
+++ NextTask/Data/TaskRepository.cs
@@ -5,7 +5,31 @@ using System.Text;
 
 namespace NextTask.Data
 {
-    class TaskRepository
+    public static class TaskRepository
     {
+        /*
+        public static TaskSet GetTasks()
+        {
+
+
+        }
+
+        public static void InsertTask(Task t)
+        {
+
+        }
+
+        public static void UpdateTask(Task t)
+        {
+
+        }
+
+        public static void DeleteTask(int id)
+        {
+
+        }
+
+        */
+
     }
 }
